d352161b7a792a8c891bcf82980b127e3e9da999
VLx2000 <victor.antunes.2000@gmail.com>
2021-10-12 15:55:05 -0300

    sync time

--- script.js
+++ script.js
@@ -1,44 +1,51 @@
 setInterval(function colorClock() {
 
-    let clock = new Date()
-    let hr = clock.getHours()
-    let min = clock.getMinutes()
-    let sec = clock.getSeconds()
-    let total_seconds = (hr * 3600) + (min * 60) + sec
-    if (sec < 10) {
-        sec = '0' + sec
-    }
-    if (min < 10) {
-        min = '0' + min
-    }
-    if (hr < 10) {
-        hr = '0' + hr
-    }
+    let time = getTime()
+    let total_seconds = (time.hr * 3600) + (time.min * 60) + time.sec
 
     hex_hours = Math.floor(total_seconds / 5400)
-    //console.log(hex_hours, 'hr')
+
     total_seconds = total_seconds % 5400
     hex_maximes = Math.floor(total_seconds / 337.5)
-    //console.log(hex_maximes,'max')
+
     total_seconds = total_seconds % 337.5
-    hex_minutes = Math.floor(total_seconds / 21.1)
-    //console.log(hex_minutes,'min')
+    hex_minutes = Math.floor(total_seconds / 21.09375)
+
     total_seconds = total_seconds % 21.1
-    hex_seconds = Math.floor(total_seconds / 1.32)
+    hex_seconds = Math.floor(total_seconds / 1.318359375)
 
     let hex_time = (hex_hours.toString(16) + '_' + hex_maximes.toString(16) + hex_minutes.toString(16) + '_' + hex_seconds.toString(16)).toUpperCase()
     let hex = hex_time.replace(/_/g, '') + '00'
     let comp_hex = complementary_color(hex)
 
-    document.getElementById('clock').textContent = hr + ':' + min + ':' + sec
-    document.getElementById('hexTime').textContent = hex_time
+    document.getElementById('hexTime').textContent = hex_time.slice(0, -2)
     document.getElementById('hexColor').textContent = 'Hex Color: #' + hex
     document.getElementById('compColor').textContent = 'Complementary Color: ' + comp_hex
 
     document.body.style.background = '#' + hex
     document.getElementById('flex1').style.color = comp_hex
     
+},1318.359375)
+
+setInterval(function Clock24Hour() {
+    let time = getTime()
+    if (time.sec < 10) {
+        time.sec = '0' + time.sec
+    }
+    if (time.min < 10) {
+        time.min = '0' + time.min
+    }
+    if (time.hr < 10) {
+        time.hr = '0' + time.hr
+    }
+    document.getElementById('clock').textContent = time.hr + ':' + time.min + ':' + time.sec
 },1000)
+
+function getTime(){
+    let clock = new Date()
+    let hr = clock.getHours(), min = clock.getMinutes(), sec = clock.getSeconds()
+    return { hr, min, sec }
+}
 
 function complementary_color(hex) {
     let hex_comp = (parseInt('FFFFFF', 16) - parseInt(hex, 16)).toString(16)
